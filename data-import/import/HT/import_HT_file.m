dab5c7ef6a536998ff50dffecd36923611e90f4f
brendan-busch <brendan.busch@uwa.edu.au>
2024-06-13 10:00:32 +0800

    Updated plotting

--- data-import/import/HT/import_HT_file.m
+++ data-import/import/HT/import_HT_file.m
@@ -1,12 +1,12 @@
 function data = import_HT_file(filename)
-
-fid = fopen(filename,'rt')
-x  = 5;
-textformat = [repmat('%s ',1,x)];
-datacell = textscan(fid,textformat,...
-    'Headerlines',1,...
-    'Delimiter',',');
-fclose(fid);
+% 
+% fid = fopen(filename,'rt')
+% x  = 5;
+% textformat = [repmat('%s ',1,x)];
+% datacell = textscan(fid,textformat,...
+%     'Headerlines',1,...
+%     'Delimiter',',');
+% fclose(fid);
 
 data = readtable(filename);
 
--- data-import/import/HT/process_HT_data.m
+++ data-import/import/HT/process_HT_data.m
@@ -25,6 +25,8 @@ saldata = import_HT_file(filename);
 outtime = datenum(2018,01,01):01:datenum(2024,04,01);
 
 fdata = interp1(flowdata.mtime,flowdata.m3,outtime);
+fdataML = interp1(flowdata.mtime,flowdata.Var4,outtime);
+
 tdata = interp1(tempdata.mtime,tempdata.Var4,outtime);
 sdata = interp1(saldata.mtime,saldata.Var4,outtime);
 fdata(fdata<0) = 0;
@@ -34,5 +36,15 @@ for i = 1:length(outtime)
     fprintf(fid,'%s,%4.4f,%4.4f,%4.4f\n',datestr(outtime(i),'dd/mm/yyyy HH:MM'),fdata(i),sdata(i),tdata(i));
 end
 fclose(fid);
+figure
 
+yyaxis left;
+plot(outtime,fdataML);hold on
+set(gca,'ylim',[0 500],'ytick',[0 100 200 300 400 500]);
+ylabel('Flow (ML/hour)');
+yyaxis right;
+plot(outtime,fdata);hold on
+set(gca,'ylim',[0 200],'ytick',[0 50 100 150 200]);
+ylabel('Flow (m3/s)');
 
+datetick('x','mm-yyyy');
